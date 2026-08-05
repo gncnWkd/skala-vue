44760118afe74ef9ac68b891f99279a3b1a13dfa
gncnWkd <hyunchan7764@naver.com>
2026-08-05 14:40:27 +0900

    fix3

--- skala-vue/src/views/NotFoundView.vue
+++ skala-vue/src/views/NotFoundView.vue
@@ -11,7 +11,7 @@ const handleGoHome = () => {
     
     <el-result class="container" icon="warning" title="페이지를 찾을 수 없습니다" sub-title="요청하신 주소가 존재하지 않거나 아직 개발되지 않았습니다.">
         <template #extra>
-            <img src="../assets/제목.png"><br/>
+            <img src="../assets/hoochoo.png"><br/>
             <el-button type="primary" @click="handleGoHome">날씨 메인으로 이동</el-button>
         </template>
     </el-result>
